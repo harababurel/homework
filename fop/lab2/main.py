6da053c5018ecfcd5e7bf9e19c41eb6dba09b183
Sergiu <srg.pscs@gmail.com>
2015-10-11 20:25:27 +0000

    shell barely functional

--- fop/lab2/main.py
+++ fop/lab2/main.py
@@ -1,14 +1,16 @@
 import pickle
 
+v = []
 
 def showPrompt():
-    print("Please enter a command. Try \"help\" if you need it.")
+    print("Please enter a command. Try \"help\".")
     print("> ", end="")
 
+def getInput():
     command = input().split()
 
     if len(command) == 0:
-        # do nothing
+        showPrompt()
         return
     if command[0] == 'insert':
         argCount = len(command) - 1
@@ -23,21 +25,52 @@ def showPrompt():
             raise(Exception("Error: the score must be a real number between 0.0 and 10.0"))
 
 
+        position = None
         if argCount == 2:
             try:
                 position = int(command[2])
-                assert(
+                assert(1 <= position) # TODO: position <= sizeOfList
+            except:
+                raise(Exception("Error: if you want to pass a custom position, make sure it's an integer between 1 and the total number of participants"))
+
+        try:
+            add(score, position)
+        except:
+            raise(Exception("Something went wrong :(. Could not add participant."))
+    elif command[0] == 'help':
+        showHelp()
+        showPrompt()
+    elif command[0] == '':
+        showPrompt()
+    else:
+        print("Command not recognized. Try \"help\".")
+        print("> ", end='')
 
 
+def add(score, position):
+    global v
 
-
-
+    if position:
+        v.insert(position-1, score)
+    else:
+        v.append(score)
 
 
 def showHelp():
     print("These are the possible commands:")
-    print("    insert X - adds a new participant with score X"
+    print("    help - displays this prompt")
+    print("    insert X - adds a new participant with score X")
     print("    insert X Y - adds a new participant with score X at position Y")
     print("    remove X - removes participant at position X")
     print("    remove X Y - removes participants with positions between X and Y")
     print("    replace X Y - replaces the score of the participant at position X with the score Y")
+
+
+def main():
+    showPrompt()
+    while True:
+        getInput()
+
+
+if __name__ == '__main__':
+    main()
