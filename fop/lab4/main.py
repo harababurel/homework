3f111fa044602513be1151ddac803b0444184d4e
Sergiu <srg.pscs@gmail.com>
2015-10-25 18:14:20 +0200

    added base for fop lab4

---
+++ fop/lab4/main.py
@@ -0,0 +1,32 @@
+"""
+    Module imports everything and runs
+    the main loop of the application.
+"""
+
+from backend import *
+from interface import *
+from persistence import *
+from tests import *
+
+
+def main():
+    try:
+        testEverything()
+        print("All tests passed :).")
+    except Exception as e:
+        print(e)
+        print("Terminating execution.")
+        exit(0)
+
+    history = restoreSession()
+    showPrompt()
+
+    while True:
+        try:
+            history = getInput(history)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
---
+++ fop/lab4/tests.py
@@ -0,0 +1,192 @@
+"""
+    Module provides test functions for the back-end methods
+    implemented elsewhere.
+"""
+from backend import *
+
+def testAdd():
+    assert add([[]], 5, None) == [[5]]
+    assert add([[100, 54], [100, 54, 32]], 1, 2) == [[100, 54], [100, 1, 54, 32]]
+    assert add([[1, 2, 3, 4]], 5, 4) == [[1, 2, 3, 5, 4]]
+
+    try: # some invalid commands
+        add([[]], 5, 9)
+        assert False
+    except:
+        pass
+
+    try:
+        add([[]], -5)
+        assert False
+    except:
+        pass
+
+    try:
+        add([[0]], 'asdf')
+        assert False
+    except:
+        pass
+
+
+def testRemove():
+    assert remove([[1, 2, 3, 4]], 1, 4) == [[]]
+    assert remove([[1, 2, 3, 4]], 2, 3) == [[1, 4]]
+    assert remove([[1], [1, 100]], 2, 2) == [[1], [1]]
+
+    try:
+        remove([[1, 2, 3, 4]], 5, 100)
+        assert False
+    except:
+        pass
+
+    try:
+        remove([[]], 1, 1)
+        assert False
+    except:
+        pass
+
+    try:
+        remove([[], [], []], 'lica', 'samadaul')
+        assert False
+    except:
+        pass
+
+
+def testReplaceScore():
+    assert replaceScore([[1]], 1, 100) == [[100]]
+    assert replaceScore([[2], [2, 2]], 2, 100) == [[2], [2, 100]]
+    assert replaceScore([[69, 69, 68, 69]], 3, 69) == [[69, 69, 69, 69]]
+
+    try:
+        replaceScore([[]], 1, 13)
+        assert False
+    except:
+        pass
+
+    try:
+        replaceScore([[1, 23]], 3, 100)
+        assert False
+    except:
+        pass
+
+    try:
+        replaceScore([[100]], 1, 101)
+        assert False
+    except:
+        pass
+
+
+def testGetAverage():
+    assert getAverage([[1, 2, 3, 4]], 1, 4) == 2.5
+    assert getAverage([[1, 2, 3]], 1, 3) == 2.0
+    assert getAverage([[1], [100, 0]], 1, 2) == 50.0
+
+    try:
+        getAverage([[]], 1, 1)
+        assert False
+    except:
+        pass
+
+    try:
+        getAverage([[1]], 'mama', 'tata')
+        assert False
+    except:
+        pass
+
+    try:
+        getAverage([[1, 2, 3, 4, 5, 6]], 1, 7)
+        assert False
+    except:
+        pass
+
+
+def testGetMinScore():
+    assert getMinScore([[1, 2, 3, 4]], 1, 4) == 1
+    assert getMinScore([[1, 2, 3]], 1, 3) == 1
+    assert getMinScore([[1], [100, 0]], 1, 2) == 0
+
+    try:
+        getMinScore([[]], 1, 1)
+        assert False
+    except:
+        pass
+
+    try:
+        getMinScore([[1, 2, 3]], 0, 2)
+        assert False
+    except:
+        pass
+
+
+def testGetMaxScore():
+    assert getMaxScore([[1, 2, 3, 4]], 1, 4) == 4
+    assert getMaxScore([[1, 2, 3]], 1, 3) == 3
+    assert getMaxScore([[1], [100, 0]], 1, 2) == 100
+
+    try:
+        getMaxScore([[]], 1, 1)
+        assert False
+    except:
+        pass
+
+    try:
+        getMaxScore([[1, 2, 3]], 0, 2)
+        assert False
+    except:
+        pass
+
+
+def testGetMulMask():
+    assert getMulMask([[10, 50, 51, 52]], 10, 1, 4) == [True, True, False, False]
+    assert getMulMask([[10, 50, 51, 52]], 3, 1, 4) == [False, False, True, False]
+    assert getMulMask([[]], 100, 1, 1) == []
+
+
+def testEverything():
+    problem = None
+
+    try:
+        testAdd()
+    except:
+        print("<add> method failed tests :(")
+        problem = True
+
+
+    try:
+        testRemove()
+    except:
+        print("<remove> method failed tests :(")
+        problem = True
+
+    try:
+        testReplaceScore()
+    except:
+        print("<replaceScore> method failed tests :(")
+        problem = True
+
+    try:
+        testGetAverage()
+    except:
+        print("<getAverage> method failed tests :(")
+        problem = True
+
+    try:
+        testGetMinScore()
+    except:
+        print("<getMinScore> method failed tests :(")
+        problem = True
+
+    try:
+        testGetMaxScore()
+    except:
+        print("<getMaxScore> method failed tests :(")
+        problem = True
+
+    try:
+        testGetMulMask()
+    except:
+        print("<getAverage> method failed tests :(")
+        problem = True
+
+    if problem is not None:
+        raise(Exception("Some tests failed :("))
