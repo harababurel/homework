45c1cfa35f1b2b587e82ba1a78067fff714f1427
Sergiu <srg.pscs@gmail.com>
2015-10-31 17:14:07 +0200

    implemented part of getInput()

---
+++ fop/lab5/settings.py
@@ -0,0 +1,12 @@
+SETTINGS = {
+        'validCommands': [
+            '',
+            'help',
+            'exit'
+            ],
+        'neededArgs': {
+            '': [0],
+            'help': [0],
+            'exit': [0]
+            }
+        }
--- fop/lab5/ui/FacultyApplication.py
+++ fop/lab5/ui/FacultyApplication.py
@@ -4,6 +4,7 @@
     students and assignments of a faculty.
 """
 from controllers.FacultyController import FacultyController
+from settings import SETTINGS
 
 class FacultyApplication():
     def __init__(self):
@@ -12,7 +13,28 @@ class FacultyApplication():
     def run(self):
         while True:
             self.showPrompt()
-            self.getInput()
+            self.command = self.getInput()
+
+            self.commandArgs = self.command.split()[1:]
+            self.command = self.command.split()[0]
+
+            if not self.command in SETTINGS['validCommands']:
+                print("%s: command not found. Try 'help'." % self.command)
+                continue
+
+            if not len(self.commandArgs) in SETTINGS['neededArgs'][self.command]:
+                print("Error: <%s> takes %r arguments (%i provided)." %
+                        (
+                            self.command,
+                            SETTINGS['neededArgs'][self.command],
+                            len(self.commandArgs)
+                            )
+                        )
+                continue
+
+            print("Command recognized, but not implemented yet.")
+
+            
 
     def showPrompt(self):
         print(">", end='')
