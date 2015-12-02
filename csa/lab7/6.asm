0142a9761ffd2062a4e88ad479030dfbba91b59c
Sergiu <srg.pscs@gmail.com>
2015-12-02 11:19:16 +0200

    finished csa lab7 homework. needs some more comments

--- csa/lab7/6.asm
+++ csa/lab7/6.asm
@@ -3,8 +3,10 @@
 
 assume cs:code, ds:data
 data segment
-    s    dw 12345, 20778, 4596, 0
-    newS db 14 dup(0)
+    s         dw  12345,20778,4596
+    dimS      equ ($-s)/2
+    newS      db  14 dup(0)
+    stackSize db  0
 data ends
 
 code segment
@@ -13,20 +15,48 @@ start:
     mov ds,ax
 
     mov si,offset s
+    add si,(dimS-1)*2    ;now ds:si should point to the last element of s
 
+    std				;the direction flag is 1, so right to left
+
+    mov cx,dimS     ;prepare the loop counter
+    jcxz sfarsit    ;if cx is 0, don't execute the loop
     bucla:
-        lodsw       ;ax = the current element of s
+        lodsw       ;ax = the current element of s  
+       
+        imparteli:
+            mov dx,0    ;extend ax to a doubleword
+            cmp ax,0
+		 je sfarsitImparteli
 
-        cmp ax,0    ;if the element 0 is reached
-        je sfarsit  ;then the string is finished
+            mov bx,10
+		 div bx	;now ax=quotient, dx=last digit
 
-        mov dx,0    ;extend ax to a doubleword
+		 push dx   ;push the last digit onto the stack
+            inc stackSize
+	   jmp imparteli
 
+        sfarsitImparteli:
+    loop bucla
 
+    mov ch,0
+    mov cl,stackSize  ;prepare the loop counter
+    mov si,offset newS
+    tiparitoareStiva:
+        pop dx
 
+        mov newS[si],dl	;create the new string
+        inc si
+
+        add dl,'0'		;also print it to stdout
+        mov ah,2           ;for debugging   
+        int 21h
+    loop tiparitoareStiva
+    
 
     sfarsit:
         mov ax,4C00h
         int 21h
 code ends
 end start
+
