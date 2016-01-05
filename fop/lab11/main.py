afac22c1095f61835ad63718ced50dfae5cde6eb
Sergiu <srg.pscs@gmail.com>
2016-01-05 09:33:28 +0200

    forgot what i changed

--- fop/lab11/main.py
+++ fop/lab11/main.py
@@ -55,29 +55,31 @@ def backIter(v, level, stack):
                 stack.pop()
 
 
-#backIter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, [])
-
-print("Rec:")
-back([2346, 654920, 2342, 1511, 654], 0, [])
-print("Iter:")
-backIter([2346, 654920, 2342, 1511, 654], 0, [])
-
-print()
-print("Rec:")
-back([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, [])
-print("Iter:")
-backIter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, [])
-
-for test in range(10):
-    n = randint(2, 10)
-    v = list(set([(randint(1, 10), randint(10, 100), randint(100, 1000))[randint(0, 2)] for i in range(n)]))
-
-    print("Test #%i: %r" % (test+1, v))
-
+def tester():
     print("Rec:")
-    back(v, 0, [])
-    print()
-
+    back([2346, 654920, 2342, 1511, 654], 0, [])
     print("Iter:")
-    backIter(v, 0, [])
+    backIter([2346, 654920, 2342, 1511, 654], 0, [])
+
     print()
+    print("Rec:")
+    back([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, [])
+    print("Iter:")
+    backIter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 0, [])
+
+    for test in range(10):
+        n = randint(2, 10)
+        v = list(set([(randint(1, 10), randint(10, 100), randint(100, 1000))[randint(0, 2)] for i in range(n)]))
+
+        print("Test #%i: %r" % (test+1, v))
+
+        print("Rec:")
+        back(v, 0, [])
+        print()
+
+        print("Iter:")
+        backIter(v, 0, [])
+        print()
+
+if __name__ == '__main__':
+    tester()
