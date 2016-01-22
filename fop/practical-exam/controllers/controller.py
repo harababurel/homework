638f479f75f2ffd231c1f431a9a320c5ece3bb44
Sergiu <srg.pscs@gmail.com>
2016-01-22 15:58:31 +0200

    list counter works fine when doing undo/redo
    Former-commit-id: 832d7dc0792bd0c5bccbfd6c36d02f14f07afcce
    Former-commit-id: cbe005c3f481c8dcebf471e33d09fa99f818bab8

--- fop/practical-exam/controllers/controller.py
+++ fop/practical-exam/controllers/controller.py
@@ -19,13 +19,19 @@ class Controller:
 
     def undo(self):
         if self.repo.now == 1:
-            print("Already at oldest state\n.")
+            print("Already at oldest state.\n")
         else:
             self.repo.now -= 1
+
+            if self.repo.current >= self.repo.getCurrentCategorySize():
+                self.repo.current = self.repo.getCurrentCategorySize() - 1
 
     def redo(self):
         if self.repo.now == len(self.repo.states) - 1:
             print("Already at most recent state.\n")
         else:
             self.repo.now += 1
+
+            if self.repo.current >= self.repo.getCurrentCategorySize():
+                self.repo.current = self.repo.getCurrentCategorySize - 1
 
