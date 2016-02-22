4f0691906a7c4d08a5ae320ed9dea2e3fe422b9d
Sergiu <srg.pscs@gmail.com>
2016-02-22 13:13:41 +0200

    added oop lab2 second exercise
    Former-commit-id: f145ee0d5db07ad73077e17cf2d7ef203598367f
    Former-commit-id: d4b9377e26c7df8c02a3e0c232386f3f71686057

--- sem2/oop/lab2/1.c
+++ sem2/oop/lab2/1.c
@@ -3,19 +3,41 @@
 
 char prime[nmax];
 
-/*
 typedef struct {
+    /* val[i] = the i-th value of the vector
+     * dp[i]  = the size of the longest increasing contiguous subsequence that starts at position i
+     * size   = number of elements
+     */
+
     int val[nmax];
+    int dp[nmax];
     int size;
 } vector;
-*/
 
-int cmmdc(int a, int b) {
+vector v;
+
+int gcd(int a, int b) {
+    /* Method recursively computes the greatest common divisor.
+     * Input: two positive integers, a and b.
+     * Output: an integer equal to the gcd of a and b.
+     */
+
     if(b==0) return a;
-    return cmmdc(b, a%b);
+    return (b, a%b);
 }
 
 void sieve(char prime[], int n) {
+    /* Method determines prime numbers in the interval [2, n]
+     * using the Sieve of Eratosthenes.
+     *
+     * Input: an array of chars prime[] (can contain any values, as they will be overwritten)
+     *        an integer n
+     *
+     * Output: the array prime[] will be constructed as follows:
+     *         - prime[x] = 1, if x is a prime number
+     *         - prime[x] = 0, otherwise
+     *         for any x in [2, n].
+     */
 
     for(int i=2; i<=n; i++)
         prime[i] = 1;
@@ -37,29 +59,45 @@ void main() {
     scanf("%c", &option);
 
     if(option == 'a') {
-        printf("You chose option (a).\n");
-        printf("\n");
+        printf("You chose option (a).\n\nn = ");        // prompt for n
+        scanf("%d", &n);                                // read it
 
-        printf("n = ");
-        scanf("%d", &n);
-
-        sieve(prime, n);
+        sieve(prime, n);                                // find the primes
 
         printf("Prime numbers: ");
         for(int i=2; i<n; i++)
             if(prime[i])
-                printf("%d ", i);
+                printf("%d ", i);                       // show them
         printf("\n");
 
     }
     else if(option == 'b') {
         printf("You chose option (b).\n");
+
+        printf("n = ");                                 // prompt for n
+        scanf("%d", &v.size);                           // read it
+
+        printf("values (separated by space): ");
+        for(int i=1; i<=v.size; i++)
+            scanf("%d", &v.val[i]);                     // read the vector
+
+        v.dp[v.size] = 1;                               // dp[n] = 1, as there is only one sequence that starts on the last position
+        for(int i=v.size-1; i; --i)
+            v.dp[i] = (v.val[i] < v.val[i+1]? v.dp[i+1]:0) + 1;
+
+        int best_dp = 0, start = 0;
+        for(int i=1; i<=v.size; i++)
+            if(v.dp[i] > best_dp) {
+                best_dp = v.dp[i];
+                start = i;
+            }
+
+        printf("Longest increasing contiguous subsequence: ");
+        for(int i=0; i<best_dp; ++i)
+            printf("%d ", v.val[start+i]);
+        printf("\n");
+
     }
-    else {
-        printf("Invalid option.");
-    }
+    else printf("Invalid option.\n");
 
-
-
-    sieve(prime, 10);
 }
