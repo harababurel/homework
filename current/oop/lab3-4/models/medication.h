2b1e9b99328f9b7180d4380031c2a50a49e6218e
Sergiu <srg.pscs@gmail.com>
2016-03-10 23:51:59 +0200

    commented code
    Former-commit-id: 3d6e8596de6f791efedd036136f0dbcbbcc8ce4a
    Former-commit-id: 926156eb51d8d67f430530be1da935d3aff21d4a
    Former-commit-id: aa8111ce0707c36be871288e20711f9f6c908f29 [formerly 27090087fbc5cbbdf40ede5a9e2dd7b531a17eb9]
    Former-commit-id: dcb291b193b450e5a2d7e0b7f780878051ae7107
    Former-commit-id: ec806acbfd58bc8a1429f6ce0c6fb35655587b5f

--- current/oop/lab3-4/models/medication.h
+++ current/oop/lab3-4/models/medication.h
@@ -7,12 +7,26 @@ typedef struct {
     double price;
 } Medication;
 
-
+/* Method allocates memory for a Medication
+ * object and returns a pointer to it.
+ */
 Medication *medication_create(char name[], double concentration, int quantity, double price);
 
+/* Method displays the table header.
+ */
 void medication_show_header();
+
+/* Method displays an empty line
+ * in the table.
+ */
 void medication_show_middle();
+
+/* Method displays the table footer.
+ */
 void medication_show_footer();
 
+/* Method takes a pointer to a Medication
+ * and displays it as a row in a table.
+ */
 void medication_show(Medication *this);
 
