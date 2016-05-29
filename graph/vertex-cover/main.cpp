92808847d6fab0b0ed5d5320ec06bfce4f4a8d6e
Sergiu Puscas <srg.pscs@gmail.com>
2016-05-29 17:09:13 +0300

    implemented smart+dumb greedy vertex cover
    Former-commit-id: b70b837be0cd63512f7d14bccef9d51992b8b899
    Former-commit-id: a2b4fd7c15cdab2d55170055b4ea9939b2b2b705
    Former-commit-id: eeec2497096266a6a52244ecdfb0d029baaaf228 [formerly e1dae7e95733a97ec8630e4058702af5a8082828]
    Former-commit-id: 0afd60dea124f38c5f407081911159da9ef56dc0
    Former-commit-id: 3b67c92d645ac9d7db7b138a0fe3cfcd58276af5 [formerly 9145a8f1aca7deff48fb2015cc4afc2d14712a64]
    Former-commit-id: 6345d03cec25db897e6c37fc4c524bbcdc855b1f
    Former-commit-id: 7398e7cad6e2f5c1718c52f881f8e6604b0a94e6

---
+++ graph/vertex-cover/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <boost/functional/hash.hpp>
+using namespace std;
+
+const int nmax = 100005;
+
+int n, m, x, y;
+vector <int> v[nmax];
+
+
+vector <int> greedy(bool smart) {
+    unordered_set <pair <int, int>, boost::hash <pair <int, int>>> edges;
+    vector <int> vertices, ret;
+
+    for(int i=1; i<=n; i++) {
+        for(auto y:v[i]) 
+            edges.insert(make_pair(min(i, y), max(i, y)));
+        vertices.push_back(i);
+    }
+
+    if(!smart)
+        random_shuffle(vertices.begin(), vertices.end());
+    else
+        sort(vertices.begin(), vertices.end(),
+             [](const int &a, const int &b) -> bool {
+                return v[a].size() > v[b].size();
+             });
+
+    for(auto x:vertices) {
+        for(auto y:v[x])
+            if(edges.find(make_pair(min(x, y), max(x, y))) != edges.end())
+                edges.erase(edges.find(make_pair(min(x, y), max(x, y))));
+
+        ret.push_back(x);
+        if(edges.empty())
+            return ret;
+    }
+
+    return ret;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    srand((unsigned)time(0)^getpid());
+
+    cin>>n>>m;
+    for(int i=1; i<=m; i++) {
+        cin>>x>>y;
+
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+
+    vector <int> vertex_cover = greedy(true);
+
+    cout<<"Vertex cover contains "<<vertex_cover.size()<<" vertices:\n";
+    for(auto x:vertex_cover)
+        cout<<x<<" ";
+    cout<<"\n";
+
+    return 0;
+}
