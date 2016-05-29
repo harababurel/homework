699d252bd8cbb4331fdf8f3bff416b19c1df2b46
Sergiu Puscas <srg.pscs@gmail.com>
2016-05-29 18:44:10 +0300

    added vertex cover testcases
    Former-commit-id: 1b722dafc8130f88ee6ef018b4b5574d3b3346a3
    Former-commit-id: 72519a0181f22fad9104aeb46199f9e6e16a1833
    Former-commit-id: 17bb7284f1128b06ec9d88623e1d4b3797db72f0 [formerly ea22e71c24c6843f169399e6f1df027b31b1fec0]
    Former-commit-id: 57c4a7e4792eccd859eec2f725452d5d67be2ea1
    Former-commit-id: f738bcfd7e81c634b4d8263d9d17a82e4e77109f [formerly 5fa567c887dc19477797839c9576f3cc0f3ac31d]
    Former-commit-id: baa155e2b395e47fc4a5ed18d6b57687c0380853
    Former-commit-id: 3f715a1aa76f520d54c0d43bcbe9794a5341a3df

--- graph/vertex-cover/main.cpp
+++ graph/vertex-cover/main.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 
 const int nmax = 100005;
+const int samples = 10000;
 
 int n, m, x, y;
 vector <int> v[nmax];
 
-
 vector <int> greedy(bool smart) {
     unordered_set <pair <int, int>, boost::hash <pair <int, int>>> edges;
     vector <int> vertices, ret;
+    vertices.reserve(n);
 
     for(int i=1; i<=n; i++) {
-        for(auto y:v[i]) 
+        for(auto y:v[i])
             edges.insert(make_pair(min(i, y), max(i, y)));
         vertices.push_back(i);
     }
@@ -39,6 +40,27 @@ vector <int> greedy(bool smart) {
     return ret;
 }
 
+vector <int> get_best_dumb_greedy() {
+    vector <int> ret = greedy(false);
+
+    for(int i=1; i<=samples; i++) {
+        vector <int> alternative = greedy(false);
+        if(alternative.size() < ret.size())
+            ret = alternative;
+    }
+    return ret;
+}
+
+vector <int> back() {
+    vector <int> ret;
+
+    if(n > 30)
+        return ret;
+
+    //for(int conf=0; conf<
+
+}
+
 int main() {
     std::ios::sync_with_stdio(false);
     srand((unsigned)time(0)^getpid());
@@ -51,11 +73,18 @@ int main() {
         v[y].push_back(x);
     }
 
-    vector <int> vertex_cover = greedy(true);
 
-    cout<<"Vertex cover contains "<<vertex_cover.size()<<" vertices:\n";
-    for(auto x:vertex_cover)
-        cout<<x<<" ";
+    vector <int> dumb_vertex_cover = get_best_dumb_greedy();
+    vector <int> smart_vertex_cover = greedy(true);
+
+    cout<<"Dumb greedy -- ("<<dumb_vertex_cover.size()<<"): ";
+//    for(auto x:dumb_vertex_cover)
+//        cout<<x<<" ";
+    cout<<"\n";
+
+    cout<<"Smart greedy - ("<<smart_vertex_cover.size()<<"): ";
+//    for(auto x:smart_vertex_cover)
+//        cout<<x<<" ";
     cout<<"\n";
 
     return 0;
