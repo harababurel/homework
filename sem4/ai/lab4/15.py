c4975ed69a9c3b828b936ca68f88306a6ff78b90
Sergiu Puscas <srg.pscs@gmail.com>
2017-05-12 11:42:11 +0300

    improved support for multiple functions/configs in AI lab4
    Former-commit-id: 9fd535cf8b4baa1e7892c6fb131693bf5c9e3b52
    Former-commit-id: 8552849c6da4e62c0c03d2ed9d4d4fd0bf67ac8c [formerly 80e5aab18264fa44c91764092305a7c9ccd2675f] [formerly 96d844a136479913c543771af42c63d7ccabb7ee [formerly 59d4f44de21b540651f23e5a44b97749a6fb31b1]]
    Former-commit-id: 00df1a3d7b9b29c720c5a214b4014937327a1f72 [formerly 9331ab73bc862af0b40f3332d5334e9832d59732]
    Former-commit-id: 1c68f77f5050ab2f837b6d53791a285b1f42ee4f
    Former-commit-id: 49290479d708c5baf21d5efe03d67c39a2205d72
    Former-commit-id: e5e3c8e7e38b36c671c6cf8993ab51c0ff815062 [formerly 36a35553bd915b2a7d6c99ef0694cdf7e64c1fad] [formerly 05a9f5a767fb443cd720922933d7d6f0035a3f34 [formerly f1747c29e93d43c97f31cae34405368a341c99b6]]
    Former-commit-id: 50b674b3d20336fdf648b127195e7fb544afe3ed [formerly 7597e3f019392e38d7036cc8fac390b6b972c427]
    Former-commit-id: 4d54bd014a2d9aa76c7d24cbe2adff61852cda9a
    Former-commit-id: 41db695cb99a1a0fea6e6a5c25a3094c19136484 [formerly e532d676178c7ecb78c0b0d97771c648d30e1d8c]
    Former-commit-id: 5219ead7e6ea3d7d3b63a0ebf1b2ecc85ae73a42
    Former-commit-id: f8c29525c45c7e30d9537283971abfa5242569a7

--- sem4/ai/lab4/15.py
+++ sem4/ai/lab4/15.py
@@ -27,31 +27,45 @@ while a termination criterion is not met do:
 """
 
 class Problem:
-    xMin = -1.5
-    xMax = 4
+    functions = {
+            'McCormick': {
+                'xMin': -1.5,
+                'xMax': 4,
+                'yMin': -3,
+                'yMax': 4,
+                'population': 200,
+                'maxAttempts': 500,
+                'omega': 0.02,
+                'phiP': 0.06 * 2,
+                'phiG': 0.03 * 2,
+                'particleSize': 12,
+                'f': lambda p: sin(p[0] + p[1]) + (p[0] - p[1])**2.0 - 1.5 * p[0] + 2.5 * p[1] + 1.0
+                },
 
-    yMin = -3
-    yMax = 4
+            'Cross-in-tray': {
+                'xMin': -10,
+                'xMax': 10,
+                'yMin': -10,
+                'yMax': 10,
+                'population': 300,
+                'maxAttempts': 10000,
+                'omega': 0.01,
+                'phiP': 0.06 * 3,
+                'phiG': 0.03 * 3,
+                'particleSize': 5,
+                'f': lambda p: -0.0001 * (abs(sin(p[0]) * sin(p[1]) * exp(100 - sqrt(p[0]**2 + p[1]**2) / pi)) + 1) ** 0.1
+                },
 
-    xBound = abs(xMax - xMin)
-    yBound = abs(yMax - yMin)
+            }
 
-    population = 200
-    maxAttempts = 500
+    # config = functions['McCormick']
+    config = functions['Cross-in-tray']
 
-    omega = 0.02
-    phiP = 0.06 * 2
-    phiG = 0.03 * 2
+    xBound = abs(config['xMax'] - config['xMin'])
+    yBound = abs(config['yMax'] - config['yMin'])
 
     clearBetweenIterations = False
 
-    # def f(position):
-    #     x, y = position
-    #     return -0.0001 * (abs(sin(x) * sin(y) * exp(100 - sqrt(x**2 + y**2) / pi)) + 1) ** 0.1
-
-    def f(position):
-        return sin(position[0] + position[1]) + (position[0] - position[1])**2.0 \
-               - 1.5 * position[0] + 2.5 * position[1] + 1.0
 
 class Particle:
     def __init__(self, swarm=None):
@@ -66,8 +80,8 @@ class Particle:
                 (self.position[0], self.position[1], self.velocity[0], self.velocity[1])
 
     def setRandomPosition(self):
-        x = uniform(Problem.xMin, Problem.xMax)
-        y = uniform(Problem.yMin, Problem.yMax)
+        x = uniform(Problem.config['xMin'], Problem.config['xMax'])
+        y = uniform(Problem.config['yMin'], Problem.config['yMax'])
 
         self.position = (x, y)
 
@@ -78,9 +92,9 @@ class Particle:
         self.velocity = (dx, dy)
 
     def updateVelocity(self, dimension, p, g):
-        newComponent = Problem.omega * self.velocity[dimension] \
-                     + Problem.phiP * p * (self.bestPosition[dimension] - self.position[dimension]) \
-                     + Problem.phiG * g * (self.swarm.bestPosition[dimension] - self.position[dimension])
+        newComponent = Problem.config['omega']* self.velocity[dimension] \
+                     + Problem.config['phiP']* p * (self.bestPosition[dimension] - self.position[dimension]) \
+                     + Problem.config['phiG']* g * (self.swarm.bestPosition[dimension] - self.position[dimension])
 
         if dimension == 0:
             self.velocity = (newComponent, self.velocity[1])
@@ -88,8 +102,8 @@ class Particle:
             self.velocity = (self.velocity[0], newComponent)
 
     def updatePosition(self):
-        newX = max(min(self.position[0] + self.velocity[0], Problem.xMax), Problem.xMin)
-        newY = max(min(self.position[1] + self.velocity[1], Problem.yMax), Problem.yMin)
+        newX = max(min(self.position[0] + self.velocity[0], Problem.config['xMax']), Problem.config['xMin'])
+        newY = max(min(self.position[1] + self.velocity[1], Problem.config['yMax']), Problem.config['yMin'])
 
         self.position = (newX, newY)
 
@@ -101,21 +115,21 @@ class Particle:
 class Swarm:
     def __init__(self, population):
         self.particles = [Particle(swarm=self) for _ in range(population)]
-        self.bestPosition = min([x.position for x in self.particles], key=lambda x: Problem.f(x))
+        self.bestPosition = min([x.position for x in self.particles], key=lambda x: Problem.config['f'](x))
 
 
     def simulate(self):
         attempts = 0
-        while attempts <= Problem.maxAttempts:
+        while attempts <= Problem.config['maxAttempts']:
             attempts += 1
 
             if Problem.clearBetweenIterations:
                 plt.clf()
 
-            plt.axis([Problem.xMin, Problem.xMax, Problem.yMin, Problem.yMax])
+            plt.axis([Problem.config['xMin'], Problem.config['xMax'], Problem.config['yMin'], Problem.config['yMax']])
             plt.scatter([particle.position[0] for particle in self.particles], \
                         [particle.position[1] for particle in self.particles], \
-                        s=12)
+                        s=Problem.config['particleSize'])
             plt.pause(0.0001)
 
 
@@ -127,10 +141,10 @@ class Swarm:
                     particle.updateVelocity(dimension, p, g)
                 particle.updatePosition()
 
-                if Problem.f(particle.position) < Problem.f(particle.bestPosition):
+                if Problem.config['f'](particle.position) < Problem.config['f'](particle.bestPosition):
                     particle.updateBestPosition()
 
-                    if Problem.f(particle.bestPosition) < Problem.f(self.bestPosition):
+                    if Problem.config['f'](particle.bestPosition) < Problem.config['f'](self.bestPosition):
                         self.bestPosition = particle.bestPosition # (particle.bestPosition[0], particle.bestPosition[1])
 
             print("BEST POSITION SO FAR: (%.3f, %.3f)" % self.bestPosition)
@@ -141,10 +155,10 @@ def main():
     # config = {
     #         'n': Problem.population,
 
-    swarm = Swarm(Problem.population)
+    swarm = Swarm(Problem.config['population'])
     swarm.simulate()
 
-    print("Found best position = (%.2f, %.2f) after %i iterations" % (swarm.bestPosition[0], swarm.bestPosition[1], Problem.maxAttempts))
+    print("Found best position = (%.2f, %.2f) after %i iterations" % (swarm.bestPosition[0], swarm.bestPosition[1], Problem.config['maxAttempts']))
 
 
 if __name__ == '__main__':
