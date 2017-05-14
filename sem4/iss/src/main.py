e330179093682d4ffd5191bf2c4fb1f3a9101be7
Sergiu Puscas <srg.pscs@gmail.com>
2017-05-14 14:58:58 +0300

    improved ISS tinder client - now it shows matches
    Former-commit-id: 4a5bcac3b3c904dad6949dbc07111e9a0931877b
    Former-commit-id: c9d602cc8d38d13131c5169d865faa711f7d8920 [formerly 120ba3d0575a85d2364630602404b635a8098f49] [formerly c543cf3e664594d0d32aa65dda8226564f2891db [formerly 404ec62361af624ceee436a902e26dc7b378d701]]
    Former-commit-id: 03949be861b000dae9759f5f16a583a3d870e6b0 [formerly 5f70a4b522a5465106a30dfabb971d1f59c29694]
    Former-commit-id: 91dfa487d2e709ea4781961ad14a55c0ec993002
    Former-commit-id: 436d1dd49a21b4e37629bd00838be6bbabc9781e
    Former-commit-id: 82155e13013c05fe6629ca8160d0a7ad518028a7 [formerly 087966e50a97c18946dfdbdf1674cda2a7887192] [formerly 69e57369e8e9ac49db37651affecb52ba8150d8e [formerly ea38c9d8f6161e51a89139c9204c63a23c429844]]
    Former-commit-id: b1e037c5bb7a9132d00355e5bc7192518c1cbb80 [formerly 65306b9f222295d1781c8c84557da92d7ed70628]
    Former-commit-id: 7a11325f4b06ec31b65c0c03739576c4eb577efc
    Former-commit-id: 333d495231c2108aa1924dee2427a5f25f6f411e [formerly 915fd6f9c196a31c10044236e99a746398f4cc95]
    Former-commit-id: 68389dbe503cfeec2293c381c489ab27e3efaef2
    Former-commit-id: 37a1ece0be919f2075779d7e5b9f2eca772073c4

--- sem4/iss/src/main.py
+++ sem4/iss/src/main.py
@@ -2,12 +2,15 @@
 
 import itertools
 import pynder
+import pickle
 
-from flask import Flask, request, session, escape, render_template, abort, redirect, url_for
+from flask import Flask, request, session, g, escape, render_template, abort, redirect, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 
 from PIL import Image
 
 import re
+import requests
 import robobrowser
 import urllib.request
 import shutil
@@ -18,14 +21,17 @@ from subprocess import call
 
 
 app = Flask(__name__)
-app.secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+app.debug = True
 
+app.secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+toolbar = DebugToolbarExtension(app)
 
 FBID = "xxxxxxxxxxxxxxx"
 FBUSER = "xxxxxxxxxxxxxxxx@gmail.com"
 FBPASS = "xxxxxxxxxxxxx"
 PHOTO_DIR = 'tmp/'
 
+"""
 def get_filename_from_url(url):
     return url.split("/")[-1]
 
@@ -54,27 +60,17 @@ def show_photos_with_feh(files):
     command = "feh -F -d " + ' '.join(files)
     call(command, shell=True)
 
-def get_access_token(email, password):
-    MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19"
-    FB_AUTH = "https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd"
-    s = robobrowser.RoboBrowser(user_agent=MOBILE_USER_AGENT, parser="lxml")
-    s.open(FB_AUTH)
-    ##submit login form##
-    f = s.get_form()
-    f["pass"] = password
-    f["email"] = email
-    s.submit_form(f)
-    ##click the 'ok' button on the dialog informing you that you have already authenticated with the Tinder app##
-    f = s.get_form()
-    s.submit_form(f, submit=f.submit_fields['__CONFIRM__'])
-    ##get access token from the html response##
-    access_token = re.search(r"access_token=([\w\d]+)", s.response.content.decode()).groups()[0]
-    #print  s.response.content.decode()
-    return access_token
+def getFacebookIDFromUsername(username):
+    try:
+        r = requests.post(
+                "http://findmyfbid.com",
+                data={'url': "https://www.facebook.com/%s" % username}
+                )
 
-def create_pynder_session(FBID, FBTOKEN):
-    session = pynder.Session(facebook_id=FBID, facebook_token=FBTOKEN)
-    return session
+        if r.status_code == 200 and r.reason == 'OK' and 'success' in r.url:
+            return ''.join([x for x in r.url if '0' <= x and x <= '9'])
+    except:
+        return None
 
 def vote_once(session):
     print("Remaining likes: %i" % session.likes_remaining)
@@ -155,11 +151,77 @@ def read_credentials():
     return (FBUSER, FBPASS)
 
 
+"""
+
+def get_access_token(email, password):
+    MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19"
+    FB_AUTH = "https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&display=touch&state=%7B%22challenge%22%3A%22IUUkEUqIGud332lfu%252BMJhxL4Wlc%253D%22%2C%220_auth_logger_id%22%3A%2230F06532-A1B9-4B10-BB28-B29956C71AB1%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=30F06532-A1B9-4B10-BB28-B29956C71AB1&ext=1470840777&hash=AeZqkIcf-NEW6vBd"
+    s = robobrowser.RoboBrowser(user_agent=MOBILE_USER_AGENT, parser="lxml")
+    s.open(FB_AUTH)
+    ##submit login form##
+    f = s.get_form()
+    f["pass"] = password
+    f["email"] = email
+    s.submit_form(f)
+    ##click the 'ok' button on the dialog informing you that you have already authenticated with the Tinder app##
+    f = s.get_form()
+    s.submit_form(f, submit=f.submit_fields['__CONFIRM__'])
+    ##get access token from the html response##
+
+    access_token = re.search(r"access_token=([\w\d]+)", s.response.content.decode()).groups()[0]
+    #print  s.response.content.decode()
+    return access_token
+
+def create_pynder_session(FBTOKEN, FBID=None):
+    session = pynder.Session(facebook_id=FBID, facebook_token=FBTOKEN)
+    return session
+
+
+
 @app.route("/")
 def index():
     if 'username' in session:
-        return "logged in as %s. <a href=\"%s\">log out</a>" % (escape(session['username']), url_for("logout"))
+        out = "<body>logged in as %s.<br><a href=\"%s\">log out</a><br>" % \
+                (escape(session['username']), url_for("logout"))
+
+        # pynder_session = create_pynder_session(session['access_token'])
+        pynder_session = load_pynder_session(session['access_token'])
+
+        if pynder_session is None:
+            print("pynder_session is None")
+        else:
+            print("pynder sesions is %r" % pynder_session)
+
+        if pynder_session is not None:
+            out += "current matches:\n"
+            out += "<ul>"
+            for match in pynder_session.matches():
+                out += "<li>%s</li>" % match
+
+            out += "</ul>"
+
+        out += "</body>"
+
+        return out
+
     return redirect(url_for('login'))
+
+def dump_pynder_session_to_file(access_token):
+    with open('sessions/%s' % access_token, 'wb') as g:
+        pickle.dump(create_pynder_session(access_token), g)
+        print("dumped session to file")
+
+def load_pynder_session(access_token):
+    try:
+        with open('sessions/%s' % access_token, 'rb') as f:
+            pynder_session = pickle.load(f)
+    except:
+        dump_pynder_session_to_file(access_token)
+        with open('sessions/%s' % access_token, 'rb') as f:
+            pynder_session = pickle.load(f)
+
+    return pynder_session
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -169,7 +231,6 @@ def login():
             password = request.form['password']
 
             access_token = get_access_token(username, password)
-            print("Access token is %s" % access_token)
 
             session['username'] = username
             session['access_token'] = access_token
@@ -181,11 +242,11 @@ def login():
     else:
         if 'username' in session:
             return redirect(url_for('index'))
-        return render_template('login.html')
+        else:
+            return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it's there
     session.pop('username', None)
     return redirect(url_for('index'))
 
