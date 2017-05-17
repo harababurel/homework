ac254fa721b70e4d5d545f24eda6f269a9a9aedf
Sergiu Puscas <srg.pscs@gmail.com>
2017-05-17 17:23:18 +0300

    implemented new ISS homepage + matches list + navbar
    Former-commit-id: 477de0dbef48b2b5bb3f7e9a10f98cd132f4dac1
    Former-commit-id: d7fa6019261e7f03c33c66d8caba37630aaef305 [formerly fe5813bf939672431bf708c7ccbd46897f7b0406] [formerly af195762d1e5372634fae6d5ae65f57e8a05c950 [formerly 40742ce6724df5408234bc734e3df7f1d5ee4701]]
    Former-commit-id: 139a8d52fdc83f0f25a48110b08360e01028b0b2 [formerly 9e76066d4f3e6fd46aed33b3ada308c93c857210]
    Former-commit-id: 8ddd739c9d0b92826da1d5ec2a48f4193d5e9c5c
    Former-commit-id: 9c3211b46d0c67cbf0bd61d8f6220e5cdccdc1c8
    Former-commit-id: d5f22c3647131a5d593c5941d4f0c095f40c9d25 [formerly ff1b9ad678de27400a914401cb11f1f53da6e68f] [formerly d6c4d2fb43a0a5c91489cb1bc2a8cd23e01564c0 [formerly 2e52d8ca01d0de0151cb499fd973c27f7e68821d]]
    Former-commit-id: 1acc51958d4ba167218745652b1b76fa6b80a596 [formerly 83ef1b17d047785e3bb619a6071403f60f34323e]
    Former-commit-id: 5ff40f8801c40ff3c6b55c9f8eeeda2c9d57ae67
    Former-commit-id: 08a0d8e7e9b31859fb7526c8bf020aa291a79253 [formerly 710a4bba5405a7a3ee43b9585c781faaebc0873f]
    Former-commit-id: d8c037c5ffa019ad03286759c71d2d05687e9fb1
    Former-commit-id: ede875be40e64282db9387295b884b5b44b7aaf5

--- sem4/iss/src/main.py
+++ sem4/iss/src/main.py
@@ -22,7 +22,7 @@ from subprocess import call
 
 
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 
 app.secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 toolbar = DebugToolbarExtension(app)
@@ -31,6 +31,9 @@ FBID = "xxxxxxxxxxxxxxx"
 FBUSER = "xxxxxxxxxxxxxxxx@gmail.com"
 FBPASS = "xxxxxxxxxxxxx"
 PHOTO_DIR = 'tmp/'
+SHOWN_MATCHES = 100
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 app.config['SOCIAL_FACEBOOK'] = {
     # 'consumer_key': '1827578677482273',
@@ -86,133 +89,125 @@ def facebook_authorized(resp):
 
     return redirect(next_url)
 
-@app.route("/logout")
-def logout():
-    pop_login_session()
-    return redirect(url_for('index'))
 
+# def get_filename_from_url(url):
+#     return url.split("/")[-1]
 
-"""
-def get_filename_from_url(url):
-    return url.split("/")[-1]
+# def download_photo(url):
+#     filename = get_filename_from_url(url)
+#     filepath = os.path.join(PHOTO_DIR, filename)
 
-def download_photo(url):
-    filename = get_filename_from_url(url)
-    filepath = os.path.join(PHOTO_DIR, filename)
+#     with urllib.request.urlopen(url) as response, open(filepath, 'wb') as out_file:
+#         shutil.copyfileobj(response, out_file)
 
-    with urllib.request.urlopen(url) as response, open(filepath, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
+# def create_image_objects(files):
+#     images = []
+#     for filename in files:
+#         images.append(Image.open(filename))
+#     return images
 
-def create_image_objects(files):
-    images = []
-    for filename in files:
-        images.append(Image.open(filename))
-    return images
+# def show_photos(images):
+#     for image in images:
+#         image.show()
 
-def show_photos(images):
-    for image in images:
-        image.show()
+# def close_photos(images):
+#     for image in images:
+#         image.close()
 
-def close_photos(images):
-    for image in images:
-        image.close()
+# def show_photos_with_feh(files):
+#     command = "feh -F -d " + ' '.join(files)
+#     call(command, shell=True)
 
-def show_photos_with_feh(files):
-    command = "feh -F -d " + ' '.join(files)
-    call(command, shell=True)
+# def getFacebookIDFromUsername(username):
+#     try:
+#         r = requests.post(
+#                 "http://findmyfbid.com",
+#                 data={'url': "https://www.facebook.com/%s" % username}
+#                 )
 
-def getFacebookIDFromUsername(username):
-    try:
-        r = requests.post(
-                "http://findmyfbid.com",
-                data={'url': "https://www.facebook.com/%s" % username}
-                )
+#         if r.status_code == 200 and r.reason == 'OK' and 'success' in r.url:
+#             return ''.join([x for x in r.url if '0' <= x and x <= '9'])
+#     except:
+#         return None
 
-        if r.status_code == 200 and r.reason == 'OK' and 'success' in r.url:
-            return ''.join([x for x in r.url if '0' <= x and x <= '9'])
-    except:
-        return None
+# def vote_once(session):
+#     print("Remaining likes: %i" % session.likes_remaining)
 
-def vote_once(session):
-    print("Remaining likes: %i" % session.likes_remaining)
+#     try:
+#         person = list(itertools.islice(session.nearby_users(), 0, 1))[0]
+#     except:
+#         print("Could not get any nearby persons.")
+#         exit(1)
 
-    try:
-        person = list(itertools.islice(session.nearby_users(), 0, 1))[0]
-    except:
-        print("Could not get any nearby persons.")
-        exit(1)
+#     print("ID: %s" % person.id)
+#     print("Name: %s" % person.name)
+#     print("Birthdate: %s (%i years old)" % (str(person.birth_date).split()[0], person.age))
+#     print("Dist: %.0f km" % person.distance_km)
+#     if person.instagram_username is not None:
+#         print("Instagram: %s" % person.instagram_username)
 
-    print("ID: %s" % person.id)
-    print("Name: %s" % person.name)
-    print("Birthdate: %s (%i years old)" % (str(person.birth_date).split()[0], person.age))
-    print("Dist: %.0f km" % person.distance_km)
-    if person.instagram_username is not None:
-        print("Instagram: %s" % person.instagram_username)
+#     if person.bio.strip() not in [None, '']:
+#         print("Bio: %s" % person.bio)
 
-    if person.bio.strip() not in [None, '']:
-        print("Bio: %s" % person.bio)
+#     if person.schools != []:
+#         sep = ' ' if len(person.schools) == 1 else '\n'
+#         print("Schools:%s%s" % (sep, sep.join(person.schools)))
 
-    if person.schools != []:
-        sep = ' ' if len(person.schools) == 1 else '\n'
-        print("Schools:%s%s" % (sep, sep.join(person.schools)))
+#     if person.jobs != []:
+#         sep = ' ' if len(person.jobs) == 1 else '\n'
+#         print("Jobs:%s%s" % (sep, sep.join(person.jobs)))
 
-    if person.jobs != []:
-        sep = ' ' if len(person.jobs) == 1 else '\n'
-        print("Jobs:%s%s" % (sep, sep.join(person.jobs)))
+#     for photo_url in person.photos:
+#         try:
+#             download_photo(photo_url)
+#         except:
+#             print("Could not download photo. Skipping.")
 
-    for photo_url in person.photos:
-        try:
-            download_photo(photo_url)
-        except:
-            print("Could not download photo. Skipping.")
+#     photo_filenames = map(lambda url: os.path.join(PHOTO_DIR, get_filename_from_url(url)), person.photos)
+#     # images = create_image_objects(photo_filenames)
 
-    photo_filenames = map(lambda url: os.path.join(PHOTO_DIR, get_filename_from_url(url)), person.photos)
-    # images = create_image_objects(photo_filenames)
+#     # show_photos(images)
+#     show_photos_with_feh(photo_filenames)
 
-    # show_photos(images)
-    show_photos_with_feh(photo_filenames)
+#     match = None
+#     while True:
+#         print("Like? (y/n/s): ", end='')
+#         decision = input()
 
-    match = None
-    while True:
-        print("Like? (y/n/s): ", end='')
-        decision = input()
+#         if decision == 'y':
+#             match = person.like()
+#             break
+#         elif decision == 'n':
+#             match = person.dislike()
+#             break
+#         elif decision == 's':
+#             try:
+#                 match = person.superlike()
+#             except Exception as e:
+#                 print(e)
+#                 continue
+#             break
 
-        if decision == 'y':
-            match = person.like()
-            break
-        elif decision == 'n':
-            match = person.dislike()
-            break
-        elif decision == 's':
-            try:
-                match = person.superlike()
-            except Exception as e:
-                print(e)
-                continue
-            break
+#     print("Match: %r" % match)
 
-    print("Match: %r" % match)
+#     # close_photos(images)
+#     print()
 
-    # close_photos(images)
-    print()
+# def show(x):
+#     print("\n".join(dir(x)))
 
-def show(x):
-    print("\n".join(dir(x)))
+# def show_messages(session, index):
+#     print('\n'.join(map(lambda x: x.__repr__(), list(itertools.islice(session.matches(), 0, 100))[index].messages)))
 
-def show_messages(session, index):
-    print('\n'.join(map(lambda x: x.__repr__(), list(itertools.islice(session.matches(), 0, 100))[index].messages)))
+# def send_message(session, index, message):
+#     print(list(itertools.islice(session.matches(), 0, 100))[index].message(message))
 
-def send_message(session, index, message):
-    print(list(itertools.islice(session.matches(), 0, 100))[index].message(message))
+# def read_credentials():
+#     FBUSER = input("Email: ")
+#     FBPASS = getpass.getpass("Pass: ")
 
-def read_credentials():
-    FBUSER = input("Email: ")
-    FBPASS = getpass.getpass("Pass: ")
+#     return (FBUSER, FBPASS)
 
-    return (FBUSER, FBPASS)
-
-
-"""
 
 def get_access_token(email, password):
     MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; U; en-gb; KFTHWI Build/JDQ39) AppleWebKit/535.19 (KHTML, like Gecko) Silk/3.16 Safari/535.19"
@@ -241,39 +236,20 @@ def create_pynder_session(FBTOKEN, FBID=None):
 @app.route("/")
 def index():
     if 'username' in session:
-        pynder_session = load_pynder_session(session['access_token'])
-        current_matches = list(itertools.islice(pynder_session.matches(), 0, 2))
-
-        matched_users = [x.user for x in current_matches]
-
-        return render_template("matches.html", session=session, matched_users=matched_users)
-
-        """
-        out = "<body>logged in as %s.<br><a href=\"%s\">log out</a><br>" % \
-                (escape(session['username']), url_for("logout"))
-
-        # pynder_session = create_pynder_session(session['access_token'])
-        pynder_session = load_pynder_session(session['access_token'])
-
-        if pynder_session is None:
-            print("pynder_session is None")
-        else:
-            print("pynder sesions is %r" % pynder_session)
-
-        if pynder_session is not None:
-            out += "current matches:\n"
-            out += "<ul>"
-            for match in pynder_session.matches():
-                out += "<li>%s</li>" % match
-
-            out += "</ul>"
-
-        out += "</body>"
-
-        return out
-        """
+        return redirect(url_for('matches'))
     else:
-        return redirect(url_for('login'))
+        return render_template('index.html')
+        # return redirect(url_for('login'))
+
+@app.route("/matches")
+def matches():
+    pynder_session = load_pynder_session(session['access_token'])
+    current_matches = list(itertools.islice(pynder_session.matches(), 0, SHOWN_MATCHES))
+
+    matched_users = [x.user for x in current_matches]
+
+    return render_template("matches.html", session=session, matched_users=matched_users)
+
 
 def dump_pynder_session_to_file(access_token):
     with open('sessions/%s' % access_token, 'wb') as g:
@@ -304,10 +280,10 @@ def login():
             session['username'] = username
             session['access_token'] = access_token
 
-            return "Logged in as %s with the following access token:<br>%s<br><a href=\"%s\">Go back</a>" % (username, access_token, url_for("index"))
+            return redirect(url_for('index'))
 
         except Exception as e:
-            return "Could not get access token. Reason:<br>%s" % e
+            return render_template("base.html", error="Could not get access token. %s" % e)
     else:
         if 'username' in session:
             return redirect(url_for('index'))
@@ -318,12 +294,10 @@ def login():
 def fb():
     return render_template('fb.html')
 
-"""
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
-"""
 
 @app.errorhandler(404)
 def page_not_found(error):
