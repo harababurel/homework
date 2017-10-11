14a1ca7d32301356f9c66e0868af29d5d913b894
Sergiu Puscas <srg.pscs@gmail.com>
2017-10-11 11:02:24 +0300

    Add the NTL library as a new build target
    Former-commit-id: 0b040a6d35026cb8cdbf10c863806af455f83d7d
    Former-commit-id: a7be3872db458e68cec3ac97af4d45957459997f [formerly 3e96b73386b7559aaed1191682212001d8768b2b] [formerly 4248d29fce17470747e636b3156264fe991b7e2c [formerly 986ed1e15874ab3faf1c975320bbdedadbd7aff3]]
    Former-commit-id: d6772daffc65b034f47603c7e75d66495d1212f4 [formerly 65a111d9c45edf0f2d4596d261c60852a7b33545]
    Former-commit-id: 55ed342c04babe3733d7945e6484caa3054ec861
    Former-commit-id: ba48f57aeb247a926254a82a25904c538605123a
    Former-commit-id: 79a9bd61512e1928008e46e695559a1236ae0669 [formerly 860af41ae0979f6acdabcaa0f5970c8a3bb29247] [formerly 2d9b73c3d0de830e685314257c194fe3987e1c90 [formerly 90befe983923c1b36246f41c3a9ae467718c13b8]]
    Former-commit-id: 46269787811e02a13a46786caa2f767b2bfc140a [formerly 2092e0c56acf66ced2bd6021cd1e9cd5492d7f99]
    Former-commit-id: 3641046e4b38013f7109cf261b1d30b4567788cf
    Former-commit-id: 26181e6cd092e00a079ac533dbd7051f5a2533f6 [formerly b340e8cbbf0286c7525569528a06cf38ba009a94]
    Former-commit-id: 8c22006cf169848c97f344af31f0d6cec71d75f5
    Former-commit-id: c87fa88e204f2c13a58d8ace508511b6ccf566b1

--- sem5/pkc/main.cc
+++ sem5/pkc/main.cc
@@ -4,9 +4,20 @@
 #include "cipher/hill.h"
 #include "ui/cipher_widget.h"
 
+#include <NTL/ZZ.h>
 #include <Eigen/Dense>
 
 int main(int argc, char *argv[]) {
+  NTL::ZZ x, a(2), n(27);
+  x = NTL::InvMod(a, n);  // functional form
+
+  std::stringstream buffer;
+  buffer << x;
+
+  printf("%s\n", buffer.str().c_str());
+
+  return 0;
+
   QApplication app(argc, argv);
   CipherWidget widget;
   widget.show();
