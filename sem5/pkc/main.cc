965ad89e30b7550dc44b2a0691a6b5c0d7bedf6e
Sergiu Puscas <srg.pscs@gmail.com>
2017-10-13 14:19:35 +0300

    Add custom theme for PKC project
    Former-commit-id: e274426178c2ce9d6ac651d4584c29144660865e
    Former-commit-id: 308fffbcbfa4997374364f02d50e2ca7ec2e9f2d [formerly 378f51c1dd999a2ee730dc282d5426c5956ee35b] [formerly 97ea98c1fb8c37e7162515f3d681144786f46483 [formerly d2fc6d8de3b44518f49fbe47346a84d3f624df77]]
    Former-commit-id: a28cce15a1f3b8205abcc199dff8add1b7c69b61 [formerly fbc4431dc678ce197c365f6530ef52d6ad8f2cbc]
    Former-commit-id: cb36d046e1f23bfc841a5c2a605a5f9466758100
    Former-commit-id: 475975cebf67089fef5dce63c4a5b03c35a5a370
    Former-commit-id: 200d164ef4b483c45a14ce3807aeae4cce2088b8 [formerly 83c11dc570472c0ce0043badc131d64a2e517ee4] [formerly 78e049aed9cf951162ebd954c9a6a057e6a719f8 [formerly 5b1d55599c0774073399a38d016cd8cee3beba1d]]
    Former-commit-id: 05c2f18fce77c45cff4fcbb9af4f46e36750536f [formerly c5014e3e1fe7d776aa19223aa1d7adca634b5574]
    Former-commit-id: 573a51e5fd72ff5887cbbaf3488b5b4e2587262c
    Former-commit-id: adef073c0d7431b950026fa9a5d9670c0a3098cd [formerly ab85b75c2982735acaf49e3506881404478d2742]
    Former-commit-id: a4aaebbd25ed1b6fa9cf75ce02caf351291278ea
    Former-commit-id: 4b248a81768f6ae6249e81b027df877a44b446e6

--- sem5/pkc/main.cc
+++ sem5/pkc/main.cc
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <qt/QtCore/QFile>
+#include <qt/QtCore/QTextStream>
 #include <qt/QtWidgets/QApplication>
+#include <qt/QtWidgets/QProxyStyle>
+#include <qt/QtWidgets/QStyleFactory>
 #include "cipher/caesar.h"
 #include "cipher/hill.h"
 #include "ui/cipher_widget.h"
@@ -8,7 +12,17 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   CipherWidget widget;
-  widget.show();
+  QApplication::setStyle("Breeze");
 
+  /* QFile f("qdarkstyle/style.qss"); */
+  /* if (!f.exists()) { */
+  /*   printf("Unable to set stylesheet, file not found\n"); */
+  /* } else { */
+  /*   f.open(QFile::ReadOnly | QFile::Text); */
+  /*   QTextStream ts(&f); */
+  /*   app.setStyleSheet(ts.readAll()); */
+  /* } */
+
+  widget.show();
   return app.exec();
 }
